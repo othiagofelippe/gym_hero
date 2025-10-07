fd3fd20dfdc40e3c1ab7a029d9dc835e6e49461a
öthiagofelippe <thiagofelippe135@gmail.com>
2025-10-07 00:49:16 -0300

    feat: adjustments to tailwind and schemas

--- schemas/auth/forgot-password.schema.ts
+++ schemas/auth/forgot-password.schema.ts
@@ -1,7 +1,7 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 export const forgotPasswordSchema = z.object({
-  email: z.string().email('Email inválido').min(1, 'Email é obrigatório'),
+  email: z.email({ message: "Email inválido" }).min(1, "Email é obrigatório"),
 });
 
 export type ForgotPasswordFormData = z.infer<typeof forgotPasswordSchema>;
