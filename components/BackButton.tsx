ec744fe5d5560afb1c05b0de05e36bab426f1957
öthiagofelippe <thiagofelippe135@gmail.com>
2025-10-10 19:59:08 -0300

    feat: add back button component

---
+++ components/BackButton.tsx
@@ -0,0 +1,26 @@
+import { router } from "expo-router";
+import { ArrowLeft } from "lucide-react-native";
+import { Pressable, View } from "react-native";
+
+export function BackButton() {
+  return (
+    <Pressable
+      onPress={() => router.back()}
+      className="self-start -ml-2 mb-6 active:opacity-60"
+    >
+      {({ pressed }) => (
+        <View
+          className={`p-2 rounded-full transition-all ${
+            pressed ? "bg-background-secondary" : ""
+          }`}
+        >
+          <ArrowLeft
+            size={24}
+            className="text-text-heading"
+            color="rgb(32, 32, 36)"
+          />
+        </View>
+      )}
+    </Pressable>
+  );
+}
